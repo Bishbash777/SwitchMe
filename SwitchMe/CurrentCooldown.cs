87f488d3fba127447285169bd66dab2d3aa66df9
Jack Bishop <Administrator@WIN-RV9T648P3OV>
2019-12-23 12:35:09 +0100

    Move cooldown code into appropriate class.

--- SwitchMe/CurrentCooldown.cs
+++ SwitchMe/CurrentCooldown.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Torch.Commands;
+using VRage.Game.ModAPI;
 
 namespace SwitchMe {
 
     public class CurrentCooldown {
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private readonly SwitchMePlugin Plugin;
+        private readonly CommandContext Context;
+
+        public CurrentCooldown(SwitchMePlugin Plugin, CommandContext Context) {
+            this.Plugin = Plugin;
+            this.Context = Context;
+        }
 
         private long _startTime;
         private readonly long _currentCooldown;
@@ -34,6 +45,98 @@ namespace SwitchMe {
                 return 0;
 
             return (_currentCooldown - elapsedTime) / 1000;
+        }
+
+
+        public bool CheckConformation(long cost, long executingPlayerId, long playerId, string playerName, IMyCharacter character) {
+
+            var confirmationCooldownMap = Plugin.ConfirmationsMap;
+
+            if (confirmationCooldownMap.TryGetValue(executingPlayerId, out CurrentCooldown confirmationCooldown)) {
+
+                long remainingSeconds = confirmationCooldown.GetRemainingSeconds(playerName);
+
+                if (remainingSeconds > 0) {
+                    confirmationCooldownMap.Remove(executingPlayerId);
+                    return true;
+                }
+            }
+            confirmationCooldown = CreateNewCooldown(confirmationCooldownMap, executingPlayerId, Plugin.CooldownConfirmation);
+
+            Context.Respond("This action will cost " + cost + "Space credits. Are you sure you want to continue? Enter the command again within " + Plugin.CooldownConfirmationSeconds + " seconds to confirm.");
+            confirmationCooldown.StartCooldown(playerName);
+
+            return false;
+        }
+
+        public static CurrentCooldown CreateNewCooldown(Dictionary<long, CurrentCooldown> cooldownMap, long playerId, long cooldown) {
+
+            var currentCooldown = new CurrentCooldown(cooldown);
+
+            if (cooldownMap.ContainsKey(playerId))
+                cooldownMap[playerId] = currentCooldown;
+            else
+                cooldownMap.Add(playerId, currentCooldown);
+
+            return currentCooldown;
+        }
+
+        public bool Confirm(long cost) {
+
+            IMyPlayer player = Context.Player;
+
+            long playerId;
+
+            if (player == null) {
+
+                Context.Respond("Console cannot use this command!");
+                return false;
+
+            }
+            else {
+                playerId = player.IdentityId;
+            }
+
+            IMyCharacter character = player.Character;
+
+            if (character == null) {
+                Context.Respond("You have no Character currently. Make sure to spawn and be out of cockpit!");
+                return false;
+            }
+
+            var currentCooldownMap = Plugin.CurrentCooldownMap;
+
+            if (currentCooldownMap.TryGetValue(playerId, out CurrentCooldown currentCooldown)) {
+
+                long remainingSeconds = currentCooldown.GetRemainingSeconds(null);
+
+                if (remainingSeconds > 0) {
+                    Log.Info("Cooldown for Player " + player.DisplayName + " still running! " + remainingSeconds + " seconds remaining!");
+                    Context.Respond("Command is still on cooldown for " + remainingSeconds + " seconds.");
+                    return false;
+                }
+
+                currentCooldown = CreateNewCooldown(currentCooldownMap, playerId, Plugin.Cooldown);
+
+            }
+            else {
+
+                currentCooldown = CreateNewCooldown(currentCooldownMap, playerId, Plugin.Cooldown);
+            }
+
+            if (!CheckConformation(cost, playerId, playerId, Context.Player.DisplayName, character))
+                return false;
+
+            try {
+
+                Log.Info("Cooldown for Player " + player.DisplayName + " started!");
+                currentCooldown.StartCooldown(null);
+                return true;
+            }
+            catch (Exception e) {
+                Log.Error(e);
+                return false;
+            }
         }
     }
 }
--- SwitchMe/GridImporter.cs
+++ SwitchMe/GridImporter.cs
@@ -72,19 +72,16 @@ namespace SwitchMe {
 
                 /* Remapping to prevent any key problems upon paste. */
                 MyEntities.RemapObjectBuilderCollection(grids);
-
                 foreach (var grid in grids) {
 
                     if (MyEntities.CreateFromObjectBuilderAndAdd(grid, true) is MyCubeGrid cubeGrid)
                         FixOwnerAndAuthorShip(cubeGrid, playerId);
                 }
-
                 if (Plugin.Config.EnabledMirror || Plugin.Config.LockedTransfer) {
 
                     targetEntity.SetPosition(newpos);
                     Context.Respond("***Transporting***");
                 }
-
                 Context.Respond("Grid has been pulled from the void!");
 
                 return true;
@@ -109,7 +106,6 @@ namespace SwitchMe {
                     /* What else should it be? LOL? */
                     if (!(grid.GetObjectBuilder(true) is MyObjectBuilder_CubeGrid objectBuilder))
                         throw new ArgumentException(grid + " has a ObjectBuilder thats not for a CubeGrid");
-
                     objectBuilders.Add(objectBuilder);
                 }
 
@@ -127,7 +123,6 @@ namespace SwitchMe {
                     }
                 }
                 //Economy stuff
-
                 if (Plugin.Config.EnableEcon && Plugin.Config.PerTransfer && Plugin.Config.PerBlock) {
                     Log.Warn("Invalid econ setup");
                     Context.Respond("Invalid econ setup - please notify an admin.");
@@ -143,23 +138,21 @@ namespace SwitchMe {
                     Context.Player.TryGetBalanceInfo(out balance);
                     long mathResult = (balance - withdraw);
                     Log.Info("Cost of transfer for" + Context.Player.DisplayName + ": " + i);
-                    Commands commands = new Commands(Plugin, Context);
-                    if (!commands.Confirm(i)) {
+                    CurrentCooldown cooldown = new CurrentCooldown(Plugin, Context);
+                    //verify that user wants to go ahead with transfer.
+                    if (!cooldown.Confirm(i)) {
                         if (mathResult < 0) {
                             Log.Info("Cost of transfer for" + Context.Player.DisplayName + ": " + i);
                             Context.Respond("Not enough funds for transfer");
                             return false;
                         }
-
                         Context.Player.RequestChangeBalance(-withdraw);
                     }
                 }
                 MyObjectBuilder_Definitions builderDefinition = MyObjectBuilderSerializer.CreateNewObject<MyObjectBuilder_Definitions>();
                 builderDefinition.Prefabs = new MyObjectBuilder_PrefabDefinition[] { definition };
-
                 bool worked = MyObjectBuilderSerializer.SerializeXML(path, false, builderDefinition);
-
-                Log.Fatal("exported " + path + " " + worked);
+                Log.Debug("exported " + path + " " + worked);
                 return true;
 
             } catch (Exception e) {
